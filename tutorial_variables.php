5401f2016862c17e16583ce5aaa104332b1fcb78
bylmpulze <steffen.maurer1993@gmail.com>
2025-07-14 00:36:12 +0200

    Comment different codes
    Reminder

--- tutorial_variables.php
+++ tutorial_variables.php
@@ -1,9 +1,6 @@
 <?php  #php language open
 
 echo ("hello world!");
-//$x = 1+1; echo($x);
-
-
 
 function number_sum($x, $y) {
     $result = $x + $y; 
@@ -31,6 +28,12 @@ echo explain_understanding(10, 20);
 echo '<pre>';
 var_dump($variable_name);
 echo '</pre>';
+
+    include_once('config.php'); #Der Code lädt die Datenbank-Zugangsdaten aus der Datei config.php
+    $db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME); #stellt dann mit diesen Daten eine Verbindung zur MySQL-Datenbank her.
+
+$sql = "SELECT * FROM `store_types`"; #Dieser Code holt alle Einträge aus der Tabelle store_types in der Datenbank.
+$result = $db->query($sql); #speichert das Ergebnis in der Variable $result.
 
 
 #php close  ↓
