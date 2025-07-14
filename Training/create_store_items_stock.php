2ae9e1f2114d651c4bd552cfb6052eb1c5ab0706
bylmpulze <steffen.maurer1993@gmail.com>
2025-07-14 21:05:52 +0200

    Finished Script hopefully :D
    Finished Script hopefully :D

--- Training/create_store_items_stock.php
+++ Training/create_store_items_stock.php
@@ -15,6 +15,21 @@ if ($result instanceof mysqli_result) {
     $store_item_stock_Array = $result->fetch_all(MYSQLI_ASSOC);
 }
 
+$sql = "SELECT * FROM `stores`";
+$result =$db->query($sql);
+
+if ($result instanceof mysqli_result) {
+    $stores_array = $result->fetch_all(MYSQLI_ASSOC);
+}
+
+foreach ($stores_array as $stores) {
+    $name_stores = $stores ["name"];
+    echo ("name " . $name_stores . '<br>');
+    $store_type_id_stores = $stores ["store_type_id"];
+    echo ("store_type_id " . $store_type_id_stores . '<br>');
+    $settlement_id = $stores ["settlement_id"];
+}
+
 foreach ($item_types_Array as $item_type) {
     $name_item_type = $item_type["name"];
     echo ("name " . $name_item_type . '<br>');
@@ -22,8 +37,18 @@ foreach ($item_types_Array as $item_type) {
     echo ("price " . $price_item_type . '<br>'); 
     $appear_chance_item_type = $item_type["appear_chance"];
     echo ("appear chance " . $appear_chance_item_type . '<br>'); 
+    $dice_roll = rand(0, 100);
+    echo ("dice roll number " . $dice_roll .  '<br>');
+    $dice_roll_quantity = rand(0, 5);
+    echo ("dice roll number quantity " . $dice_roll_quantity .  '<br>');      
+    
+    if ($dice_roll_quantity<= $appear_chance_item_type) {
+        echo ("Quantity added");
+        $sql = "INSERT INTO `store_item_stock` (`id`, `store_id`, `item_type_id`, `quantity`) VALUES ('$item_type', '$store_type_id_stores', '$dice_roll_quantity')";
+    }
+    else {
+        echo ("no quantity added");
+    }
+}        
 
-}
-
-#Continue later
 ?>
